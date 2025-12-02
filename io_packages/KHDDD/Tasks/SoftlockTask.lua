32954dfcc9a9ade274216c471638b0cc604c6484
Lux <luxmakesgames@gmail.com>
2025-12-01 22:17:33 -0600

    Riku can now leave Beagle Boys fight

--- io_packages/KHDDD/Tasks/SoftlockTask.lua
+++ io_packages/KHDDD/Tasks/SoftlockTask.lua
@@ -8,27 +8,18 @@ function SoftlockTask:PreventSoftlocks()
 end
 
 function SoftlockTask:BeagleBoys()
-	--If riku has to leave the Beagle Boys fight via /unstuck, he is unable to reinitiate the fight
-	--Player may find themselves in this situation if they enter the fight without polespin
-
 	local _currWorld = ReadByte(MemoryAddresses.world)
 	local _currChar = getCharacter()
 
 	if _currChar == 1 and _currWorld == 0x04 then
-		--Riku is in CotM; check if he has initiated the beagle boys fight
+		--Disable flag preventing Riku from leaving the beagle boys room
 		local _storyProg = ReadByte(WorldFlags.countryOfMusketeers.riku.story+0x01)
 		if _storyProg < 0x3F and _storyProg >= 0x1F then --Player has started the fight but has not obtained the stage gadget yet
-			local _currRoom = ReadByte(MemoryAddresses.room)
-			if _currRoom == 0x03 then --Riku is in the theater. This is only possible if the player abandoned the fight
-				WriteByte(MemoryAddresses.room, 0x0B) --Move player to the machine room
-				WriteByte(MemoryAddresses.map, 0x02) --Event data for beagle boys fight
-				WriteByte(MemoryAddresses.evt, 0x02)
-				WriteByte(MemoryAddresses.btl, 0x02)
+			if ReadByte(0xA44792) == 0x0A then --Flag is enabled; disable it
+				WriteArray(0xA44792, {0x00, 0x00})
 			end
 		end
-
 	end
-
 end
 
 function SoftlockTask:LeaveMT()
